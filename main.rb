668bd33cc895e4e714e898b485c1bed6c1e39d36
devendra-alt <devendramulewa171@gmail.com>
2023-09-22 23:37:49 +0530

    Refactor main.rb

--- main.rb
+++ main.rb
@@ -2,9 +2,14 @@ require_relative 'school/person'
 require_relative 'school/capitalize_decorator'
 require_relative 'school/trimmer_decorator'
 
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+
+def main
+  person = Person.new(22, 'maximilianus')
+  person.correct_name
+  capitalized_person = CapitalizeDecorator.new(person)
+  puts capitalized_person.correct_name
+  capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+  puts capitalized_trimmed_person.correct_name
+end
+
+main()
