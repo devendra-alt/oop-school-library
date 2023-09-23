6afa71cf29d94cd8be28d6d8aacd61465f7ce935
devendra-alt <devendramulewa171@gmail.com>
2023-09-23 18:53:01 +0530

    Refactor student.rb parent_permission params

--- school/student.rb
+++ school/student.rb
@@ -2,14 +2,14 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission:parent_permission)
     @classroom = classroom
   end
 
   def add_classroom(classroom)
     @classroom = classroom
-    @classroom.students.push(self) unless classroom.students.include?(self)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
