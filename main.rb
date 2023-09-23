1c262e2708c0638a7f0b62fac7e14e8a62352b5d
devendra-alt <devendramulewa171@gmail.com>
2023-09-23 18:53:53 +0530

    Add UI operations for school-library

--- main.rb
+++ main.rb
@@ -1,6 +1,86 @@
+require_relative 'school/classroom'
 require_relative 'school/person'
+require_relative 'school/student'
+require_relative 'school/teacher'
+require_relative 'school/book'
 require_relative 'school/capitalize_decorator'
 require_relative 'school/trimmer_decorator'
+
+$classroom = ClassRoom.new('my-classroom')
+
+$books = []
+$people = []
+$rentals = []
+
+
+
+def list_books
+  $books.each_with_index do |book,index|
+    puts "#{index} Title : #{book.title}, Author : #{book.author}"
+  end
+end
+
+def list_people
+  $people.each_with_index do |person,index|
+    puts "#{index} [#{person.class}] Name: #{person.name} ID: #{person.id} Age: #{person.age}"
+  end
+end
+
+def create_person
+  print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+  user_type = gets.chomp.strip.to_i
+  if user_type==1
+    print "Age: "
+    age = gets.chomp.strip.to_i
+    print "Name: "
+    name = gets.chomp.strip
+    print 'Has parent permission? [Y/N]: '
+    permission = gets.chomp.strip.upcase == 'Y' 
+    $people << Student.new(age,name,permission)
+  else
+    print "Age: "
+    age = gets.chomp.strip.to_i
+    print "Name: "
+    name = gets.chomp.strip
+    print "Specialization: "
+    specialization =  gets.chomp.strip
+    $people << Teacher.new(age,specialization,name)
+  end
+end
+
+def add_book
+  print "Title: "
+  title = gets.chomp.strip
+  print "Author: "
+  author = gets.chomp.strip
+  $books << Book.new(title,author)
+end
+
+def create_rental
+  puts "Select a book from the following list by number [not id]"
+  list_books
+  book_choice = gets.chomp.to_i
+  book = $books[book_choice]
+  puts 'Select a person from the following list by number [not id]'
+  list_people
+  person_choice = gets.chomp.to_i
+  person = $people[person_choice]
+  print 'Enter date of booking [yyyy/mm/dd] : '
+  date = gets.chomp.strip
+  person.add_rental(date,book)
+end
+
+def list_rentals
+  print "ID of person"
+  person_id = gets.chomp.to_i
+  person = $people.each do |person|
+    return person if person.id == id 
+  end
+  puts 'Rentals: '
+  person.rentals.each do |rental|
+    puts "Date : #{rental.date}, Book : #{rental.book.title}, by #{rental.book.author}"
+  end
+end
 
 def main 
   user_input=0
